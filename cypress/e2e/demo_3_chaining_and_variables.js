35867d16d781bb48d20de7842c41ae3ced537af8
Patrick Schilder <patrick@djpasch.nl>
2023-03-02 20:47:32 +0100

    Extend example

--- cypress/e2e/demo_3_chaining_and_variables.js
+++ cypress/e2e/demo_3_chaining_and_variables.js
@@ -7,14 +7,15 @@ context('Actions', () => {
   
     it('Chaining and variables - not working', () => {
         cy.log('Mixing sync and async - not working')
-        const menu = cy.get('[class="dropdown-item"]')
+        const menu = cy.get('[class="dropdown-item"]').eq(0)
         cy.log(menu.text())
     })
 
     it('Chaining and variables - working', () => {
         cy.log('Mixing sync and async - working')
-        cy.get('[class="dropdown-item"]').then(menu => {
+        cy.get('[class="dropdown-item"]').eq(0).then(menu => {
           cy.log(menu.text())
+          expect(menu.text()).to.contain('Fruit')
         }
         )
       })
