2bf3b14b653dfb68524ad93b3f38e120f0a22d9c
Patrick Schilder <patrick@djpasch.nl>
2023-03-02 20:05:22 +0100

    Update config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,21 @@
-{
-    "$schema": "https://on.cypress.io/cypress.schema.json",
-    "viewportWidth": 1400,
-    "viewportHeight": 1050,
-    "baseUrl": "https://webshop.mobiletestautomation.nl"
-}
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1400,
+  viewportHeight: 1050,
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 4000,
+  pageLoadTimeout: 30000,
+  watchForFileChanges: false,
+  taskTimeout: 90000,
+  numTestsKeptInMemory: 30,
+  blockHosts: ['*googleapis.com', '*microsoft.com', '*cloudflare.com'],
+
+  e2e: {
+    baseUrl: 'https://webshop.mobiletestautomation.nl',
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    specPattern: ['cypress/e2e/**/*.{feature,features}','cypress/e2e/*.js'],
+  },
+});
